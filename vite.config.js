97d09d461188b51d8e95dacbe301b260012b6c90
EduArsene <eduardoanthonny283@gmail.com>
2025-07-20 20:09:36 -0500

    olvidé cambiar la ruta para poder desplegar

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // usa el nombre exacto de tu repo
+  base: '/my-lp/', // usa el nombre exacto de tu repo
 
   plugins: [react()],
 })
